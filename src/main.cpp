81974d9af714e470abab99309b1f71ea3aa87d9d
Aurorayqz <aurorayqz@foxmail.com>
2018-02-26 15:11:22 +0800

    net-work

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,13 @@ int main(int argc, char const *argv[])
     config.host_name="aurorayqz_docker";
     config.root_dir="..";
 
+    // 配置网络参数
+    config.ip = "192.168.0.100"; 		// 容器 IP
+    config.bridge_name = "docker0";     // 宿主机网桥
+    config.bridge_ip   = "192.168.0.1"; // 宿主机网桥 IP
+
+    
+
     docker::container container(config);// 根据 config 构造容器
     container.start();// 启动容器
     std::cout<<"stop container..."<<std::endl;
